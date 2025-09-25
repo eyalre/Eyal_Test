f8a2f54dd2b741e880c308490b05df0d64dfc992
eyalre <eyalre@gmail.com>
2025-09-25 15:43:28 +0300

    add print

--- Scripts/hello.py
+++ Scripts/hello.py
@@ -6,6 +6,8 @@ user_input = input("Enter numbers separated by spaces: ")
 # Convert input string to a list of integers
 numbers = [int(num) for num in user_input.split()]
 
+print ("You entered:", numbers)
+
 # Sort the numbers
 sorted_numbers = sorted(numbers)
 
